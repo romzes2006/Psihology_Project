cc9c5b662866f42ca8ef832c3e7c227d0cf3ccd3
Roman Sitnikov <romzes2006@gmail.com>
2021-11-15 18:36:36 +0800

    Внесены изменения в поведения форм при закрытии.

--- PsihologARM/Team_climate_frm.xaml.cs
+++ PsihologARM/Team_climate_frm.xaml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Windows;
 
 namespace PsihologARM
@@ -10,6 +11,19 @@ namespace PsihologARM
         }
 
         private void Btn_close_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            this.Owner.Show();
+            // Возможно, правильнее isClosing = !e.Cancel;
+            //  если выдаете запрос на закрытие или что-то типа того
+        }
+
+        private void Btn_GoBack_OnClick(object sender, RoutedEventArgs e)
         {
             this.Close();
         }

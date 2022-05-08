a2aea56c16ef47fc755e88c0e815a2dc5f6cafd6
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2022-05-08 13:38:17 +0400

    протестировал методы GetStan и CorrectAnswer

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,12 +2,15 @@
 using PTestLib.Utils;
 
 var json = File.ReadAllText("Questions.json");
+var jsonClient = File.ReadAllText("ClientAnswers.json");
 
 ListQuestions questions = JsonSerialize.GetQuestions(json);
+ListQuestions clientAnswers = JsonSerialize.GetQuestions(jsonClient);
 
 AizenecTest aizenecTest = new AizenecTest(questions);
 
-aizenecTest.GetStan(questions);
+
+aizenecTest.GetStan(clientAnswers);
 
 Console.WriteLine("Dostovernost\t"+aizenecTest.dostovernostBal);
 Console.WriteLine("Extraversia\t"+aizenecTest.extraversiaBal);
--- PTestLib/AizenecTest.cs
+++ PTestLib/AizenecTest.cs
@@ -12,7 +12,7 @@ namespace PTestLib
         public ListInterpretations Interpritations;
         private byte[] _dostovernost = new byte[] { 6, 24, 36, 12, 18, 30, 42, 48, 54 };
         private byte[] _extraversia = new byte[] { 1, 3, 8, 10, 13, 17, 22, 25, 27, 39, 44, 46, 49, 53, 56, 5, 15, 20, 29, 32, 34, 37, 41, 51 };
-        private byte[] _neirotizm = new byte[] { 2, 4, 7, 9, 11, 14, 16, 19, 21, 23, 26, 28, 31, 33, 35, 38, 40, 43, 45, 47, 50, 52, 55, 57 };
+        private byte[] _neirotizm = new byte[]   { 2, 4, 7, 9, 11, 14, 16, 19, 21, 23, 26, 28, 31, 33, 35, 38, 40, 43, 45, 47, 50, 52, 55, 57 };
 
         public byte dostovernostBal = 0;
         public byte extraversiaBal = 0;
@@ -58,7 +58,7 @@ namespace PTestLib
                 {
                     foreach (var anCl in answersClient)
                     {
-                        if (anCl.Content == answer.Content)
+                        if (anCl.Content.ToString().Contains(answer.Content.ToString()))
                         {
                             correctAnswers.Add(Questions.Get(i).Id);
                         }
